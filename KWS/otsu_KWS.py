30e69b5ca99e982064e21e93695906f529b3ba1b
Mauro Gwerder <mauro.gwerder@students.unibe.ch>
2020-04-23 13:20:31 +0200

    init

---
+++ KWS/otsu_KWS.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from skimage import filters
+from os import listdir
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+
+MYPATH = 'C:/Users/mg_gw/OneDrive/Dokumente/UNIBE 8. Semester/3_Pattern_Recognition/32_exercises/03_KWS/PatRec17_KWS_Data-master/PatRec17_KWS_Data-master/images/'
+OUTPATH = MYPATH + '/binarized/'
+files = filenames = [f for f in listdir(MYPATH)if isfile(join(MYPATH, f))]  # extracts all filenames from folder
+for file in files:
+    img = plt.imread(MYPATH + file)
+    val = filters.threshold_otsu(img)
+
+    plt.figure(figsize=(24, 40))
+    plt.imshow(img > val, cmap='gray', interpolation='nearest')
+    plt.axis('off')
+    plt.savefig(OUTPATH + 'otsu_' + file)
+
