c36e6eaa60a300de4b98defa5cb2217689f0bb17
Mauro Gwerder <mauro.gwerder@students.unibe.ch>
2020-04-23 15:55:07 +0200

    improved scaling

--- KWS/otsu_KWS.py
+++ KWS/otsu_KWS.py
@@ -2,18 +2,16 @@ import matplotlib.pyplot as plt
 from skimage import filters
 from os import listdir
 from os.path import isfile, join
-import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+import numpy as np
+
 
 MYPATH = 'C:/Users/mg_gw/OneDrive/Dokumente/UNIBE 8. Semester/3_Pattern_Recognition/32_exercises/03_KWS/PatRec17_KWS_Data-master/PatRec17_KWS_Data-master/images/'
 OUTPATH = MYPATH + '/binarized/'
-files = filenames = [f for f in listdir(MYPATH)if isfile(join(MYPATH, f))]  # extracts all filenames from folder
+files = [f for f in listdir(MYPATH)if isfile(join(MYPATH, f))]  # extracts all filenames from folder
 for file in files:
-    img = plt.imread(MYPATH + file)
-    val = filters.threshold_otsu(img)
-
-    plt.figure(figsize=(24, 40))
-    plt.imshow(img > val, cmap='gray', interpolation='nearest')
-    plt.axis('off')
-    plt.tight_layout()
-    plt.savefig(OUTPATH + 'otsu_' + file)
+    img = Image.open(MYPATH + file)
+    val = filters.threshold_otsu(np.asarray(img))
+    newIm = Image.fromarray(img > val)
+    newIm.save(OUTPATH + 'otsu_' + file)
 
